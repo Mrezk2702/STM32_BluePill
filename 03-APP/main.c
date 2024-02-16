927712287e87c220f15c202127bc1d234d150520
Mohamed-Rezk <mrezk880@gmail.com>
2024-02-16 18:53:50 +0200

    Testing commit

--- 03-APP/main.c
+++ 03-APP/main.c
@@ -15,6 +15,18 @@ void main(void)
 
 	RCC_voidInitSysClock();
 
+   	RCC_ERR test=RCC_voidEnableClock(APB1, 3);
+	test=RCC_voidEnableClock(APB2, 0);
+	test=RCC_voidEnableClock(APB2, 1);
+	test=RCC_voidEnableClock(APB2, 10);
+	test=RCC_voidEnableClock(APB2, 15);
+	test=RCC_voidEnableClock(APB2, 16);
+	test=RCC_voidEnableClock(APB2, 17);
+	test=RCC_voidEnableClock(APB2, 18);
+	test=RCC_voidEnableClock(APB2, 19);
+	test=RCC_voidEnableClock(APB2, 22);
+	test=RCC_voidEnableClock(APB2, 23);
+	test=RCC_voidEnableClock(APB2, 32);
 
 	while(1)
 	{
