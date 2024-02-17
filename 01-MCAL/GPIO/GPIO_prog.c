1f83b632b3d1c974327218a36edda3ee7538aaf5
Mohamed-Rezk <mrezk880@gmail.com>
2024-02-17 17:46:16 +0200

    GPIO prepartion

---
+++ 01-MCAL/GPIO/GPIO_prog.c
@@ -0,0 +1,13 @@
+
+
+
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+#include "GPIO_private.h"
+#include "GPIO_register.h"
+#include "GPIO_config.h"
+#include "GPIO_interface.h"
+
+
+
+
---
+++ 01-MCAL/GPIO/GPIO_register.h
@@ -0,0 +1,16 @@
+#ifndef GPIO_REGISTER_H_
+#define GPIO_REGISTER_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
