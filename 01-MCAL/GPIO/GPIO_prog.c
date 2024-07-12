ad3b7aa6fec94b5d7a1ede168ea8cdcec2867f7e
Mohamed-Rezk <mrezk880@gmail.com>
2024-07-12 15:25:20 +0300

    GPIO modifications

--- 01-MCAL/GPIO/GPIO_prog.c
+++ 01-MCAL/GPIO/GPIO_prog.c
@@ -9,7 +9,8 @@
 
 #include "GPIO_interface.h"
 
-
+/*Global Variables*/
+SetPIN_Err_State Global_EnuSetPinErrorState;
 
 
 void GPIO_voidInit(void)
@@ -22,6 +23,70 @@ void GPIO_voidInit(void)
 
 	GPIO_PORTB->CRL=PORTB_CRL_INIT;
 
-
 	GPIO_PORTB->CRH=PORTB_CRH_INIT;
+}
+
+void GPIO_voidSetPinValue(PORT_INDEX Cpy_enuPortIndx,u8 Cpy_PinNumber,b Cpy_bData)
+{
+	Global_EnuSetPinErrorState=GPIO_NO_ERROR;
+	if(Cpy_PinNumber>15)
+	{
+		/*Wrong input*/
+		Global_EnuSetPinErrorState=PIN_OUT_OF_RANGE;
+	}
+	else
+	{
+		/*execute the function normally*/
+
+		switch (Cpy_enuPortIndx)
+		{
+		case PORTA:
+			if(Cpy_bData==STD_bTRUE)
+			{
+				SET_BIT(GPIO_PORTA->ODR,Cpy_PinNumber);
+			}
+			else if(Cpy_bData==STD_bFALSE)
+			{
+				CLEAR_BIT(GPIO_PORTA->ODR,Cpy_PinNumber);
+			}
+			else
+			{
+				Global_EnuSetPinErrorState=WRONG_DATA;
+			}
+			break;
+		case PORTB:
+
+			if(Cpy_bData==STD_bTRUE)
+			{
+				SET_BIT(GPIO_PORTB->ODR,Cpy_PinNumber);
+			}
+			else if(Cpy_bData==STD_bFALSE)
+			{
+				CLEAR_BIT(GPIO_PORTB->ODR,Cpy_PinNumber);
+			}
+			else
+			{
+				Global_EnuSetPinErrorState=WRONG_DATA;
+			}
+			break;
+
+		case PORTC:
+			if(Cpy_bData==STD_bTRUE)
+			{
+				SET_BIT(GPIO_PORTC->ODR,Cpy_PinNumber);
+			}
+			else if(Cpy_bData==STD_bFALSE)
+			{
+				CLEAR_BIT(GPIO_PORTC->ODR,Cpy_PinNumber);
+			}
+			else
+			{
+				Global_EnuSetPinErrorState=WRONG_DATA;
+			}
+			break;
+
+		default:
+
+		}
+	}
 }
